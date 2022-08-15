b9ee2c3934a9575b4c4ce0ce7e1134932fa85c5e
kodenamekrak <kyelodge2005@gmail.com>
2022-08-15 22:19:55 +0100

    should now disable in multi, doesnt repeatedly log

--- src/main.cpp
+++ src/main.cpp
@@ -5,165 +5,176 @@
 #include "questui/shared/BeatSaberUI.hpp"
 #include "UnityEngine/Transform.hpp"
 #include "MainConfig.hpp"
-#include "GlobalNamespace/MenuEnvironmentManager.hpp"
 #include "bs-utils/shared/utils.hpp"
-bool inMulti, score_sub;
+#include "GlobalNamespace/MultiplayerLobbyController.hpp"
+#include "GlobalNamespace/LobbySetupViewController.hpp"
+#include "GlobalNamespace/MenuEnvironmentManager.hpp"
+#include "GlobalNamespace/MultiplayerModeSelectionViewController.hpp"
 
+bool inMulti, score_sub;
+int resizeCycle;
 
 using namespace GlobalNamespace;
 using namespace UnityEngine;
-using MenuEnvironmentType = GlobalNamespace::MenuEnvironmentManager::MenuEnvironmentType;
-DEFINE_CONFIG(MainConfig)
+DEFINE_CONFIG(MainConfig);
 
 static ModInfo modInfo; // Stores the ID and version of our mod, and is sent to the modloader upon startup
 
 // Loads the config from disk using our modInfo, then returns it for use
-Configuration& getConfig() {
+Configuration &getConfig()
+{
     static Configuration config(modInfo);
     config.Load();
     return config;
 }
 
 // Returns a logger, useful for printing debug messages
-Logger& getLogger() {
-    static Logger* logger = new Logger(modInfo);
+Logger &getLogger()
+{
+    static Logger *logger = new Logger(modInfo);
     return *logger;
 }
 
-
-
-MAKE_HOOK_MATCH(Environment, &GlobalNamespace::MenuEnvironmentManager::ShowEnvironmentType, void, GlobalNamespace::MenuEnvironmentManager* self, GlobalNamespace::MenuEnvironmentManager::MenuEnvironmentType menuEnvironmentType)
+// resets our variable to only resize sabers once
+MAKE_HOOK_MATCH(ActivationResetter, &MenuEnvironmentManager::ShowEnvironmentType, void, MenuEnvironmentManager *self, MenuEnvironmentManager::MenuEnvironmentType menuEnvironmentType)
 {
-    Environment(self, menuEnvironmentType);
-    getLogger().info("environment hook");
-    switch (menuEnvironmentType) {
-        case MenuEnvironmentType::None:
-            break;
-            getLogger().info("env-none");
-        case MenuEnvironmentType::Default:
-            inMulti = false;
-            getLogger().info("env-default");
-            break;
-        case MenuEnvironmentType::Lobby:
-            inMulti = true;
-            getLogger().info("env-lobby");
-            break;
-        default:
-            getLogger().info("Default");
-            break;
+    ActivationResetter(self, menuEnvironmentType);
+    if (resizeCycle != 0)
+    {
+        resizeCycle = 0;
+        getLogger().info("Will resize on next song start");
     }
 }
-;
+
+// tells us when the player joins multiplayer to disable the mod
+MAKE_HOOK_MATCH(JoinLobbyUpdater, &LobbySetupViewController::DidActivate, void, LobbySetupViewController *self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
+{
+    JoinLobbyUpdater(self, firstActivation, addedToHierarchy, screenSystemEnabling);
+    inMulti = true;
+    getLogger().info("Joined multiplayer, disabling mod");
+}
+
+// tells us when the player leavs multiplayer to enable the mod
+MAKE_HOOK_MATCH(LeaveLobbyUpdater, &MultiplayerModeSelectionViewController::DidActivate, void, MultiplayerModeSelectionViewController *self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
+{
+    LeaveLobbyUpdater(self, firstActivation, addedToHierarchy, screenSystemEnabling);
+    if (inMulti)
+    {
+        inMulti = false;
+        getLogger().info("Left multiplayer, enabling mod");
+    }
+}
+
 // hook for changing saber size
-MAKE_HOOK_MATCH(sabersize, &Saber::ManualUpdate, void, Saber *self){
-    sabersize (self);
+MAKE_HOOK_MATCH(SaberSizeChanger, &Saber::ManualUpdate, void, Saber *self)
+{
+    SaberSizeChanger(self);
 
-
-    getLogger().info("saber hook");
-
-    getLogger().info("in multi status %d", inMulti );
-
-
-    // checks if the mod config says the mod is enabled or the player is in a multiplayer lobby
-    if (getMainConfig().Mod_active.GetValue() && !inMulti){
-        // sets the saber thickness and length based on the mod config
-        self->get_transform()->set_localScale({getMainConfig().Thickness.GetValue(), getMainConfig().Thickness.GetValue(), getMainConfig().Length.GetValue()});
-         getLogger().info("1");
-    }
-    else {
-        self->get_transform()->set_localScale({1, 1, 1});
-        getLogger().info("else");
-    }
-
-
-
-    // if the saber length is more than 1 disable score submission
-    if (getMainConfig().Mod_active.GetValue()){
-        if (getMainConfig().Length.GetValue() != 1){
-            if (score_sub == 0){
-                bs_utils::Submission::disable(modInfo);
-                score_sub = 1;
-        getLogger().info("disable score");
-    }
-    }
-    }
-    else{
-        if (score_sub == 1){
-            bs_utils::Submission::enable(modInfo);
-            getLogger().info("enable score");
-            score_sub = 0;
+    // checks if the mod config says the mod is enabled or the player is in a multiplayer lobby, only activates once
+    if (resizeCycle < 2)
+    {
+        if (getMainConfig().Mod_active.GetValue() && !inMulti)
+        {
+            // sets the saber thickness and length based on the mod config
+            self->get_transform()->set_localScale({getMainConfig().Thickness.GetValue(), getMainConfig().Thickness.GetValue(), getMainConfig().Length.GetValue()});
+            getLogger().info("Not in multiplayer, resizing saber to %fw %fl", self->get_transform()->get_localScale().x, self->get_transform()->get_localScale().z);
         }
-        
+        else
+        {
+            self->get_transform()->set_localScale({1, 1, 1});
+            getLogger().info("In multiplayer, setting sabers to default size");
+        }
+        resizeCycle++;
     }
+
+    // if the saber length is more than 1 disable score submission, not active in multiplayer as sabers are forced to default size
+    if (!inMulti)
+    {
+        if (getMainConfig().Mod_active.GetValue())
+        {
+            if (getMainConfig().Length.GetValue() != 1)
+            {
+                if (score_sub == false)
+                {
+                    bs_utils::Submission::disable(modInfo);
+                    score_sub = true;
+                    getLogger().info("Disable score");
+                }
+            }
+        }
+        else
+        {
+            if (score_sub == true)
+            {
+                bs_utils::Submission::enable(modInfo);
+                getLogger().info("Enable score");
+                score_sub = false;
+            }
+        }
     }
-    
+}
 
-    
-
-void DidActivate(HMUI::ViewController* self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
+void DidActivate(HMUI::ViewController *self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
+{
     // Create our UI elements only when shown for the first time.
-    if(firstActivation) {
+    if (firstActivation)
+    {
         // Create a container that has a scroll bar.
-        GameObject* container = QuestUI::BeatSaberUI::CreateScrollableSettingsContainer(self->get_transform());
-       
+        GameObject *container = QuestUI::BeatSaberUI::CreateScrollableSettingsContainer(self->get_transform());
+
         // Create a text that says "Hello World!" and set the parent to the container.
         QuestUI::BeatSaberUI::CreateText(container->get_transform(), "The mod config, change these settings or completely turn off the mod!");
 
         // the mod active toggle
-        QuestUI::BeatSaberUI::CreateToggle(container->get_transform(), "Mod Enabled", getMainConfig().Mod_active.GetValue(), [](bool value) {
-		getMainConfig().Mod_active.SetValue(value, true);
-        });
+        QuestUI::BeatSaberUI::CreateToggle(container->get_transform(), "Mod Enabled", getMainConfig().Mod_active.GetValue(), [](bool value)
+                                           { getMainConfig().Mod_active.SetValue(value, true); });
         // the saber length config slider
-        QuestUI::SliderSetting *sliderSetting1 = QuestUI::BeatSaberUI::CreateSliderSetting(container->get_transform(), "length", 0.01f , getMainConfig().Length.GetValue(), 0.01f, 15.0f, 1.0f, [](float value){
-            getMainConfig().Length.SetValue(value, true);
-        });
+        QuestUI::SliderSetting *sliderSetting1 = QuestUI::BeatSaberUI::CreateSliderSetting(container->get_transform(), "length", 0.01f, getMainConfig().Length.GetValue(), 0.01f, 15.0f, 1.0f, [](float value)
+                                                                                           { getMainConfig().Length.SetValue(value, true); });
         // the saber thickness slider
-        QuestUI::SliderSetting *sliderSetting2 = QuestUI::BeatSaberUI::CreateSliderSetting(container->get_transform(), "Thickness", 0.01f , getMainConfig().Thickness.GetValue(), 0.0f, 15.0f, 1.0f, [](float value){
-            getMainConfig().Thickness.SetValue(value, true);
-        });
+        QuestUI::SliderSetting *sliderSetting2 = QuestUI::BeatSaberUI::CreateSliderSetting(container->get_transform(), "Thickness", 0.01f, getMainConfig().Thickness.GetValue(), 0.0f, 15.0f, 1.0f, [](float value)
+                                                                                           { getMainConfig().Thickness.SetValue(value, true); });
 
         // reset button
 
         QuestUI::BeatSaberUI::CreateUIButton(container->get_transform(), "Reset sabers",
-            [sliderSetting1, sliderSetting2]() {
-            getMainConfig().Thickness.SetValue(1);
-            getMainConfig().Length.SetValue(1);
-            sliderSetting1->set_value(getMainConfig().Length.GetValue());
-            sliderSetting2->set_value(getMainConfig().Thickness.GetValue());
-        });
-    }       
+                                             [sliderSetting1, sliderSetting2]()
+                                             {
+                                                 getMainConfig().Thickness.SetValue(1);
+                                                 getMainConfig().Length.SetValue(1);
+                                                 sliderSetting1->set_value(getMainConfig().Length.GetValue());
+                                                 sliderSetting2->set_value(getMainConfig().Thickness.GetValue());
+                                             });
+    }
 }
 
-
 // Called at the early stages of game loading
-extern "C" void setup(ModInfo& info) {
+extern "C" void setup(ModInfo &info)
+{
     info.id = MOD_ID;
     info.version = VERSION;
     modInfo = info;
-	
+
     getConfig().Load(); // Load the config file
     getMainConfig().Init(info);
     getLogger().info("Completed setup!");
 }
 
 // Called later on in the game loading - a good time to install function hooks
-extern "C" void load() {
+extern "C" void load()
+{
     il2cpp_functions::Init();
-    
-    
+
     QuestUI::Init();
     QuestUI::Register::RegisterModSettingsViewController(modInfo, DidActivate);
     QuestUI::Register::RegisterMainMenuModSettingsViewController(modInfo, DidActivate);
 
-
     getLogger().info("Installing hooks...");
 
-    INSTALL_HOOK(getLogger(),sabersize);
-    INSTALL_HOOK(getLogger(),Environment)
-    
-    
+    INSTALL_HOOK(getLogger(), SaberSizeChanger);
+    INSTALL_HOOK(getLogger(), JoinLobbyUpdater);
+    INSTALL_HOOK(getLogger(), ActivationResetter);
+    INSTALL_HOOK(getLogger(), LeaveLobbyUpdater);
+
     getLogger().info("Installed all hooks!");
 }
-
-
-
